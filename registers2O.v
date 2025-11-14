8e68976cbbbe935b40df83cd52ab47fc66f458f3
Karan Misal <143802033+karan18misal@users.noreply.github.com>
2025-11-14 09:18:41 +0530

    Remove initial memory values from registers2O.v
    Removed initial memory values from registers2O.v.

--- registers2O.v
+++ registers2O.v
@@ -14,19 +14,7 @@ module registers(
 );
 
     reg [31:0] memory [1023:0];
-    initial begin
-  memory[0]  = 32'h00000000; // value = 0
-  memory[1]  = 32'h00000001; // value = 1
-  memory[2]  = 32'h00000002; // value = 2
-  memory[3]  = 32'h00000003; // value = 3
-  memory[4]  = 32'h00000004; // value = 4
-  memory[5]  = 32'h00000005; // value = 5
-  memory[6]  = 32'h00000006; // value = 6
-  memory[7]  = 32'h00000007; // value = 7
-  memory[8]  = 32'h00000008; // value = 8
-  memory[9]  = 32'h00000009; // value = 9
-  memory[10] = 32'h0000000A; // value = 10
-end
+    
 
 
     always @(posedge clk) begin
